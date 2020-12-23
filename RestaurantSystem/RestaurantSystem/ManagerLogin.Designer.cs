07027e33a010c5de4181e8c96718ceb332b7ba5b
BadrTarek <71422548+BadrTarek@users.noreply.github.com>
2020-12-23 17:19:36 +0200

    Manager Login

---
+++ RestaurantSystem/RestaurantSystem/ManagerLogin.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace RestaurantSystem
+{
+    partial class ManagerLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.managerName = new System.Windows.Forms.TextBox();
+            this.managerPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // exitButton1
+            // 
+            this.exitButton1.FlatAppearance.BorderSize = 0;
+            this.exitButton1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.exitButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.exitButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton1.Location = new System.Drawing.Point(744, 13);
+            this.exitButton1.Name = "exitButton1";
+            this.exitButton1.Size = new System.Drawing.Size(52, 39);
+            this.exitButton1.TabIndex = 0;
+            this.exitButton1.Text = "x";
+            this.exitButton1.UseVisualStyleBackColor = true;
+            this.exitButton1.Click += new System.EventHandler(this.exitButton1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(120, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(284, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(232, 35);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Manager Login";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(120, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(522, 294);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // managerName
+            // 
+            this.managerName.Location = new System.Drawing.Point(246, 178);
+            this.managerName.Name = "managerName";
+            this.managerName.Size = new System.Drawing.Size(178, 20);
+            this.managerName.TabIndex = 5;
+            // 
+            // managerPassword
+            // 
+            this.managerPassword.Location = new System.Drawing.Point(246, 220);
+            this.managerPassword.Name = "managerPassword";
+            this.managerPassword.PasswordChar = '#';
+            this.managerPassword.Size = new System.Drawing.Size(178, 20);
+            this.managerPassword.TabIndex = 6;
+            // 
+            // ManagerLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.managerPassword);
+            this.Controls.Add(this.managerName);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ManagerLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Manager Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox managerName;
+        private System.Windows.Forms.TextBox managerPassword;
+    }
+}
+
---
+++ RestaurantSystem/RestaurantSystem/ManagerLogin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace RestaurantSystem
+{
+    public partial class ManagerLogin : Form
+    {
+        SqlConnection con = new SqlConnection("data source=.;Initial Catalog=RestaurantSystem;Trusted_Connection=True;");
+        SqlCommand cmd;
+        public ManagerLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void clear()
+        {
+            managerName.Clear();
+            managerPassword.Clear();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if(managerName.Text.ToString() == string.Empty || managerPassword.Text.ToString() == string.Empty)
+            {
+                MessageBox.Show("Invalid Login Please Try Again !!", "Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                clear();
+            }
+            else
+            {
+                con.Open();
+                cmd = new SqlCommand("SELECT password FROM Managers WHERE name='" + managerName.Text.ToString() + "'", con);
+                SqlDataReader sdr = cmd.ExecuteReader();
+                if (sdr.HasRows)
+                {
+                    sdr.Read();
+                    if(sdr["password"].ToString()==managerPassword.Text.ToString())
+                    {
+                        // Login = true;
+                        MessageBox.Show("Loged in", "Login", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid Login Please Try Again !!", "Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        clear();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Login Please Try Again !!", "Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    clear();
+                }
+                con.Close();
+            }
+            
+        }
+    }
+}
