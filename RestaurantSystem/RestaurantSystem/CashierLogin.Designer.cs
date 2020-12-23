d3e6045c5c7bccd4ac98e99cb4bbce958cf5a2bf
BadrTarek <71422548+BadrTarek@users.noreply.github.com>
2020-12-23 18:00:29 +0200

    Cashier Login

---
+++ RestaurantSystem/RestaurantSystem/CashierLogin.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace RestaurantSystem
+{
+    partial class CashierLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton1 = new System.Windows.Forms.Button();
+            this.cashierPassword = new System.Windows.Forms.TextBox();
+            this.cashierName = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // exitButton1
+            // 
+            this.exitButton1.FlatAppearance.BorderSize = 0;
+            this.exitButton1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.exitButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.exitButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton1.Location = new System.Drawing.Point(744, 13);
+            this.exitButton1.Name = "exitButton1";
+            this.exitButton1.Size = new System.Drawing.Size(52, 39);
+            this.exitButton1.TabIndex = 1;
+            this.exitButton1.Text = "x";
+            this.exitButton1.UseVisualStyleBackColor = true;
+            this.exitButton1.Click += new System.EventHandler(this.exitButton1_Click);
+            // 
+            // cashierPassword
+            // 
+            this.cashierPassword.Location = new System.Drawing.Point(246, 220);
+            this.cashierPassword.Name = "cashierPassword";
+            this.cashierPassword.PasswordChar = '#';
+            this.cashierPassword.Size = new System.Drawing.Size(178, 20);
+            this.cashierPassword.TabIndex = 12;
+            // 
+            // cashierName
+            // 
+            this.cashierName.Location = new System.Drawing.Point(246, 178);
+            this.cashierName.Name = "cashierName";
+            this.cashierName.Size = new System.Drawing.Size(178, 20);
+            this.cashierName.TabIndex = 11;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(522, 294);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 10;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(120, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 19);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(284, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 35);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Cashier Login";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(120, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Name";
+            // 
+            // CashierLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.cashierPassword);
+            this.Controls.Add(this.cashierName);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CashierLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cashier Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton1;
+        private System.Windows.Forms.TextBox cashierPassword;
+        private System.Windows.Forms.TextBox cashierName;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
